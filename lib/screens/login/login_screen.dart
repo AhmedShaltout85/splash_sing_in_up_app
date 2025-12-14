9f0b81fa60dc10ba591597011798b46904a201ed
AhmedShaltout <eng.abozaher@gmail.com>
2025-12-15 00:03:29 +0200

    Last-Update: 2025-12-15

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:splash_sing_in_up_app/common_widgets/custom_widgets/custom_button.dart';
 import 'package:splash_sing_in_up_app/common_widgets/resuable_widgets/reusable_toast.dart';
-import 'package:splash_sing_in_up_app/newtorkl_repos/remote_repo/firebase_api_services.dart';
 import 'package:splash_sing_in_up_app/utils/app_assets.dart';
 import 'package:splash_sing_in_up_app/utils/app_colors.dart';
 
