2b3229fd559cebf82937f09cb580b4fdf99a5cac
AhmedShaltout <eng.abozaher@gmail.com>
2025-12-15 00:52:08 +0200

    Last-Update: 2025-12-15

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,10 @@ import 'package:flutter/services.dart';
 import 'package:splash_sing_in_up_app/screens/home/home_screen.dart';
 import 'package:splash_sing_in_up_app/screens/login/login_screen.dart';
 import 'package:splash_sing_in_up_app/screens/signup/signup_screen.dart';
-// import 'package:splash_sing_in_up_app/screens/login/login_screen.dart';
 import 'package:splash_sing_in_up_app/screens/splash/splash_screen.dart';
 import 'package:splash_sing_in_up_app/utils/app_route.dart';
 
 import 'firebase_options.dart';
-// import 'package:splash_sing_in_up_app/screens/splash/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
