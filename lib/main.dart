e82aee725ae39fd7328d8231bcd0f41ea88e5c03
AhmedShaltout <eng.abozaher@gmail.com>
2025-12-20 23:29:57 +0200

    Last-Update: 2025-12-20

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,17 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-// import 'package:splash_sing_in_up_app/screens/home/home_screen.dart';
-// import 'package:splash_sing_in_up_app/screens/login/login_screen.dart';
-// import 'package:splash_sing_in_up_app/screens/signup/signup_screen.dart';
-// import 'package:splash_sing_in_up_app/screens/splash/splash_screen.dart';
-// import 'package:splash_sing_in_up_app/utils/app_route.dart';
 
 import 'controller/task_provider.dart';
 import 'controller/user_provider.dart';
 import 'firebase_options.dart';
+import 'screens/home/home_screen.dart';
+import 'screens/login/login_screen.dart';
+import 'screens/signup/signup_screen.dart';
+import 'screens/splash/splash_screen.dart';
 import 'screens/task/task_screen.dart';
 import 'screens/user/user_list_screen.dart';
+import 'utils/app_route.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -39,36 +39,34 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'UI Kit App',
-      // onGenerateRoute: (settings) {
-      //   switch (settings.name) {
-      //     case AppRoute.splashRouteName:
-      //       return MaterialPageRoute(
-      //         builder: (context) => const SplashScreen(),
-      //       );
-      //     case AppRoute.loginRouteName:
-      //       return MaterialPageRoute(builder: (context) => const LoginScreen());
-      //     case AppRoute.homeRouteName:
-      //       return MaterialPageRoute(builder: (context) => const HomeScreen());
-      //     case AppRoute.signupRouteName:
-      //       return MaterialPageRoute(
-      //         builder: (context) => const SignUpScreen(),
-      //       );
-      //     case AppRoute.userRouteName:
-      //       return MaterialPageRoute(
-      //         builder: (context) => const UserListScreen(),
-      //       );
-      //     case AppRoute.taskRouteName:
-      //       return MaterialPageRoute(
-      //         builder: (context) => const TaskScreen(),
-      //       );
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case AppRoute.splashRouteName:
+            return MaterialPageRoute(
+              builder: (context) => const SplashScreen(),
+            );
+          case AppRoute.loginRouteName:
+            return MaterialPageRoute(builder: (context) => const LoginScreen());
+          case AppRoute.homeRouteName:
+            return MaterialPageRoute(builder: (context) => const HomeScreen());
+          case AppRoute.signupRouteName:
+            return MaterialPageRoute(
+              builder: (context) => const SignUpScreen(),
+            );
+          case AppRoute.userRouteName:
+            return MaterialPageRoute(
+              builder: (context) => const UserListScreen(),
+            );
+          case AppRoute.taskRouteName:
+            return MaterialPageRoute(builder: (context) => const TaskScreen());
 
-      //     default:
-      //       return MaterialPageRoute(
-      //         builder: (context) => const SplashScreen(),
-      //       );
-      //   }
-      // },
-      home: const TaskScreen(),
+          default:
+            return MaterialPageRoute(
+              builder: (context) => const SplashScreen(),
+            );
+        }
+      },
+      // home: const TaskScreen(),
     );
   }
 }
